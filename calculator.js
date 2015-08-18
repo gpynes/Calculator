3e0d10c63d1d105a2cdba26c52bf0697c1fff541
Greg Pynes Pynes <gpynes@users.noreply.github.com>
2015-08-18 15:42:35 -0700

    Buttons now click, Enter buttons retrieve input text

--- calculator.js
+++ calculator.js
@@ -1,20 +1,45 @@
-var subBttn = document.getElementsByClassName( "enter" );
+var subBttn = document.querySelectorAll( ".enter" );
 var inText = document.getElementsByTagName( "input" );
+var numbers = document.querySelectorAll( ".nums" );
+var currentNumber = 0;
+var keys = {
+	0 : 1,
+	1 : 2,
+	2 : 3,
+	3 : 4,
+	4 : 5,
+	5 : 6,
+	6 : 7,
+	7 : 8,
+	8 : 9,
+	9 : 0
+}
+
+
 
 var p = function( word ) {
 	console.log( word );
 };
 
 
-var maths =0;
-
+//Retrieves text from input box for use as string.
 function getText() {
-	console.log( "This works" );
-	return inText.input.value;
+	console.log(inText.input.value );
 }
 
-subBttn.onClick = function() {
-	console.log( "This")
-};
 
-subBttn[ 0 ].addEventListener( "click", console.log("stuff"));
+for ( var i = 0; i < numbers.length; i++ ) {
+	numbers[ i ].addEventListener( "click", (function(i) {
+        return function() { 
+            console.log(i); 
+        }; 
+    } ) ( i ) );
+}
+
+for ( var i = 0; i < subBttn.length; i++ ) {
+	subBttn[ i ].addEventListener( "click", (function(i) {
+		return function() {
+			getText();
+		}
+	} ) ( i ) );
+}
