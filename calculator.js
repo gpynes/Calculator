ac7c6c9d7e4835db050090a9e0e5b3a7664b5109
Greg Pynes Pynes <gpynes@users.noreply.github.com>
2015-08-18 15:50:24 -0700

    Buttons add value to input Text and are set to currentNumber variable.

--- calculator.js
+++ calculator.js
@@ -2,18 +2,7 @@ var subBttn = document.querySelectorAll( ".enter" );
 var inText = document.getElementsByTagName( "input" );
 var numbers = document.querySelectorAll( ".nums" );
 var currentNumber = 0;
-var keys = {
-	0 : 1,
-	1 : 2,
-	2 : 3,
-	3 : 4,
-	4 : 5,
-	5 : 6,
-	6 : 7,
-	7 : 8,
-	8 : 9,
-	9 : 0
-}
+var keys = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 ];
 
 
 
@@ -25,13 +14,23 @@ var p = function( word ) {
 //Retrieves text from input box for use as string.
 function getText() {
 	console.log(inText.input.value );
+	currentNumber = parseInt( inText.input.value );
 }
 
+//Adds button values to input box text
+function addInput( i ) {
+	inText.input.value += keys[ i ];
+}
+
+function clearInput() {
+	inText.input.value = null;
+}
 
 for ( var i = 0; i < numbers.length; i++ ) {
 	numbers[ i ].addEventListener( "click", (function(i) {
         return function() { 
             console.log(i); 
+            addInput( i );
         }; 
     } ) ( i ) );
 }
@@ -40,6 +39,7 @@ for ( var i = 0; i < subBttn.length; i++ ) {
 	subBttn[ i ].addEventListener( "click", (function(i) {
 		return function() {
 			getText();
+			clearInput();
 		}
 	} ) ( i ) );
 }
